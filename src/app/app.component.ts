75cae0ccfd83882a9dd5f2865534e076463a67aa
Carlo Röthlisberger <carlo.roethlisberger@students.bfh.ch>
2023-12-21 01:11:03 +0100

    Change to style, fade-in of header and image, smooth scrolling when clicking links in navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,41 @@ export class AppComponent {
 
   collapseSidebar() {
     this.isSidebarExpanded = false;
+  }
+
+  constructor(private renderer: Renderer2) { }
+
+  scrollToElement(targetId: string): void {
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      this.renderer.setProperty(document.documentElement, 'scrollTop', targetElement.offsetTop);
+    }
+  }
+
+  smoothScrollToElement(targetId: string): void {
+    const targetElement = document.getElementById(targetId);
+  
+    if (targetElement) {
+      const startPosition = window.pageYOffset;
+      const targetPosition = targetElement.offsetTop;
+      const distance = targetPosition - startPosition;
+      const duration = 500; // Adjust the duration as needed
+  
+      let start: number | null = null; // Explicitly declare the type of 'start'
+      const step = (timestamp: number) => {
+        if (!start) start = timestamp;
+        const progress = timestamp - start;
+        const percentage = Math.min(progress / duration, 1);
+        window.scrollTo(0, startPosition + distance * percentage);
+  
+        if (progress < duration) {
+          requestAnimationFrame(step);
+        }
+      };
+  
+      requestAnimationFrame(step);
+    }
   }
 
 
